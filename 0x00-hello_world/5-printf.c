4bc3e551b07917c1f67a259936bf54288a879d22
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-08 16:39:07 -0700

    Print betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print the string
+ * This program prints"with proper grammar,but the outcome is a piece of art,"
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+i}
