387d934bb825acf45a0cfc3fce9b6b33d66b8688
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-08 17:12:55 -0700

    print the size of various data-types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-Print the string
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	printf("Size of a char:%dbytes(s)\n", sizeof(char));
+	printf("Size of an int:%dbytes(s)\n", sizeof(int));
+	printf("Size of a long int:%dbyte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%dbyte(s)\n", sizeof(long long int));
+	printf("Size of a float:%dbyte(s)\n", sizeof(float));
+	return (0);
+}
