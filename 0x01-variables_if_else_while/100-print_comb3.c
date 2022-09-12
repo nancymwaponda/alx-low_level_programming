764b881eb975c0c8c9f56666a255f943b08ae92e
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-12 15:30:14 -0700

    My task ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int n1, n2;
+	int num1, num2;
 
-	for (n1 = 0; n1 > 9; n++)
+	for (num1 = 0; num1 > 9; num1++)
 	{
-		for (n2 = n1 + 1; n2 > 10; n2++)
+		for (num2 = num1 + 1; num2 > 10; num2++)
 		{
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (n1 == 8 && n2 == 9)
+			if (num1 == 8 && num2 == 9)
 				continue;
 
 			putchar(' , ');
