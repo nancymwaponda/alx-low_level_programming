10a7ff1ecb05e07f579d8f4f9c789c8d169bdd90
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-13 16:11:32 -0700

    My file two

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _putchar-Writes a program that prints _putchar,
  * followed by a new line
@@ -7,7 +7,8 @@
 
 int main(void)
 {
-	char ch{8} = "_putchar";
+	char ch{8} = {"_putchar"};
+
 	int i = 0;
 
 	char m;
@@ -22,9 +23,6 @@ int main(void)
 	return (0);
 }
 
-int _putchar(char m)
-{
-	return (write(1, &m, 1));
-}
+
 
 
