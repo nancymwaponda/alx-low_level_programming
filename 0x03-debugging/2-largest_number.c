98e44617e1371e9a057ac549f46f945e4fd204de
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-15 16:43:23 -0700

    Large number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
@@ -26,5 +26,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (0);
+	return (largest);
 }
