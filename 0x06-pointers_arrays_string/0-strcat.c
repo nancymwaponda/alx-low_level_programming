1d7d91e4f467941c28c185ce3b55e768d379ebfd
nancymwaponda <mwapondanancie1@gmail.com>
2022-09-22 16:12:35 -0700

    Tasks

--- 0x06-pointers_arrays_string/0-strcat.c
+++ 0x06-pointers_arrays_string/0-strcat.c
@@ -1,41 +1,75 @@
 #include "main.h"
 
+int get_length(char *s);
+
 /**
+ * _strcat - Concatenate two strings
+ * @dest: Target of concatenation
+ * @src: String to be concatenated
  *
- * _strcat - concatenates two strings
- *
- * @dest: destination char
- *
- * @src: source char
- *
- * Return: new char
- *
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 
 {
 
-	int i;
+	int initial_end;
 
-	int n = 0;
+	int j;
 
 
-		while (dest[n] != '\0')
+	initial_end = get_length(dest);
 
-	{	
+j = 0;
 
-	n++;
+		while (src[j] != '\0')
+
+	{
+
+											dest[initial_end + j] = src[j];
+
+	j++;
 
 	}
 
-		for (i = 0 ; i < n && src[i] != '\0' ; i++)
 
-		dest[n + i] = src[i];
-
-		dest[n + i] = '\0';
+	dest[initial_end + j] = '\0';
 
 
 	return (dest);
+
+}
+
+
+
+/**
+ * get_length - returns the length of a string
+ * @s: String whose length we wish to determine
+ * Return: Length of s
+ */
+
+int get_length(char *s)
+
+{
+
+	int length;
+
+int i;
+
+
+	length = 0;
+
+	i = 0;
+
+	while (s[i] != '\0')
+
+{
+	++length;
+											i++;
+
+}
+
+	return (length);
 
 }
---
+++ 0x06-pointers_arrays_string/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings of a specified number
+ * @n: number of characters
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclength = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		 srclength++;
+		 src++;
+	}
+
+	if (n > srclength)
+	{
+		n = srclength;
+	}
+
+	src = start;
+
+	for (i < n; i++)
+	{
+	*dest++ = *src++;
+	}
+
+	*dest = '\0';
+	return (temp);
+}
+		 
+
